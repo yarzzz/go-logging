922b433ea931979e7c051d6b6b98aad51aabc4b9
tongzhoujie <1036079528@qq.com>
2021-08-13 14:51:37 +0800

    init logging

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/yarzzz/go-logging
+
+go 1.16
+
+require go.uber.org/zap v1.19.0
---
+++ logging.go
@@ -0,0 +1,50 @@
+package logging
+
+import (
+	"os"
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type Logger struct {
+	*zap.SugaredLogger
+}
+
+func NewConsoleLogger(name string) *Logger {
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout(time.RFC3339)
+	logger := zap.New(
+		zapcore.NewCore(
+			zapcore.NewConsoleEncoder(encoderConfig),
+			zap.CombineWriteSyncers(
+				zapcore.AddSync(os.Stdout),
+			),
+			zapcore.InfoLevel,
+		),
+	)
+	logger = logger.Named(name)
+	return &Logger{
+		SugaredLogger: logger.Sugar(),
+	}
+}
+
+func NewFileLogger(name string, path string) *Logger {
+	f, _ := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout(time.RFC3339)
+	logger := zap.New(
+		zapcore.NewCore(
+			zapcore.NewJSONEncoder(encoderConfig),
+			zap.CombineWriteSyncers(
+				zapcore.AddSync(f),
+			),
+			zapcore.InfoLevel,
+		),
+	)
+	logger = logger.Named(name)
+	return &Logger{
+		SugaredLogger: logger.Sugar(),
+	}
+}
